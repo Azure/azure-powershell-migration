d08161f6886dedfdde6ce590649339cdd68cdef3
Keith Babinec <keith.babinec@gmail.com>
2020-08-08 14:47:54 -0700

    Added dynamic loading for newtonsoft.json

--- powershell-module/Az.Tools.Migration/Az.Tools.Migration.psd1
+++ powershell-module/Az.Tools.Migration/Az.Tools.Migration.psd1
@@ -45,9 +45,7 @@
     # RequiredModules = @()
 
     # Assemblies that must be loaded prior to importing this module
-    RequiredAssemblies = @(
-        '.\Resources\Assembly\Newtonsoft.Json.12.0.3\Newtonsoft.Json.dll'
-    )
+    # RequiredAssemblies = @()
 
     # Script files (.ps1) that are run in the caller's environment prior to importing this module.
     ScriptsToProcess = @(
--- powershell-module/Az.Tools.Migration/Az.Tools.Migration.psm1
+++ powershell-module/Az.Tools.Migration/Az.Tools.Migration.psm1
@@ -3,6 +3,19 @@
     Ensure that all of the module functionality is loaded directly from this file.
 #>
 
+# conditionally load required assemblies (in this case Newtonsoft.Json)
+# PowerShell Core has this assembly loaded by default.
+# Windows PowerShell does not ship with this so it usually needs to be loaded.
+
+$jsonType = [System.AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object { $_.GetTypes() | Where-Object { $_.FullName -eq "Newtonsoft.Json.JsonConvert" } }
+
+if ($jsonType -eq $null)
+{
+    # type is not already loaded.
+    # attempt to load this assembly from the resources folder.
+    Add-Type -Path "$PSScriptRoot\Resources\Assembly\Newtonsoft.Json.12.0.3\Newtonsoft.Json.dll" -ErrorAction Stop
+}
+
 # load classes
 
 foreach ($classFile in (Get-ChildItem -Path "$PSScriptRoot\Classes" -Recurse -Include "*.ps1"))
