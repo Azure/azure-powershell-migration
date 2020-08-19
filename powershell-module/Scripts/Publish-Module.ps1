94ab857af7dcf28dc553a26c2dbbb8d3e93fea79
dingmeng-xue <dixue@microsoft.com>
2020-08-19 23:46:06 +0800

    Package and sign powershell modules

---
+++ powershell-module/Scripts/Publish-Module.ps1
@@ -0,0 +1,32 @@
+<#
+
+.SYNOPSIS
+    Create nuget packages for module.
+
+.PARAMETER ApiKey
+    ApiKey used to publish nuget to PS repository.
+
+.PARAMETER RepositoryLocation
+    Location we want to publish too.
+#>
+param(
+    [CmdletBinding()]
+    [Parameter(Mandatory = $true)]
+    [string]$ApiKey,
+
+    [Parameter(Mandatory = $true)]
+    [string]$RepositoryLocation
+)
+try {
+    $tempRepoName = ([System.Guid]::NewGuid()).ToString()
+    Register-PSRepository -Name $tempRepoName -SourceLocation $RepositoryLocation -PublishLocation $RepositoryLocation -InstallationPolicy Trusted -PackageManagementProvider NuGet
+    $modulePath = Join-Path $RepositoryLocation Az.Tools.Migration -Resolve
+    Publish-Module -Path $modulePath -Repository $tempRepoName -NuGetApiKey $ApiKey -Force
+} catch {
+    $Errors = $_
+    Write-Error ($_ | Out-String)
+} finally {
+    Unregister-PSRepository -Name $tempRepoName
+}
+
+
