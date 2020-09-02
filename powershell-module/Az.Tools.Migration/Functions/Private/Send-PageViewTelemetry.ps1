a945359af1a80bfdb92e2784269e58385659a2aa
dingmeng-xue <dixue@microsoft.com>
2020-09-03 00:38:24 +0800

    update after review

--- powershell-module/Az.Tools.Migration/Functions/Private/Send-PageViewTelemetry.ps1
+++ powershell-module/Az.Tools.Migration/Functions/Private/Send-PageViewTelemetry.ps1
@@ -43,7 +43,8 @@ function Send-PageViewTelemetry
     )
     Process
     {
-        if($null -eq [Constants]::TelemetryClient) {
+        if($null -eq [Constants]::TelemetryClient) 
+        {
             $TelemetryClient = New-Object Microsoft.ApplicationInsights.TelemetryClient
             $TelemetryClient.InstrumentationKey = [Constants]::PublicTelemetryInstrumentationKey
             $TelemetryClient.Context.Session.Id = $CurrentSessionId
@@ -63,9 +64,11 @@ function Send-PageViewTelemetry
         $page.Properties["x-ms-client-request-id"]= [Constants]::CurrentSessionId
         $page.Properties["PowerShellVersion"]= $PSVersionTable.PSVersion.ToString();
         
-        if($null -ne $MyInvocation.MyCommand) {
+        if($null -ne $MyInvocation.MyCommand)
+        {
             $page.Properties["ModuleName"] = $MyInvocation.MyCommand.ModuleName
-            if($null -ne $MyInvocation.MyCommand.Module -and $null -ne $MyInvocation.MyCommand.Module.Version) {
+            if($null -ne $MyInvocation.MyCommand.Module -and $null -ne $MyInvocation.MyCommand.Module.Version)
+            {
                 $page.Properties["ModuleVersion"] = $MyInvocation.MyCommand.Module.Version.ToString()
             }
         }
@@ -75,19 +78,23 @@ function Send-PageViewTelemetry
         # prepare custom properties
         # convert the hashtable to a custom object, if properties were supplied.
 
-        if ($PSBoundParameters.ContainsKey('CustomProperties') -and $CustomProperties.Count -gt 0)
+        if ($PSBoundParameters.ContainsKey('CustomProperties') -and $CustomProperties.Count -gt 0) 
         {
-            foreach ($Key in $CustomProperties.Keys) {
+            foreach ($Key in $CustomProperties.Keys) 
+            {
                 $page.Properties[$Key] = $CustomProperties[$Key]
             }
         }
 
         $client.TrackPageView($page)
 
-        try {
+        try
+        {
             $client.Flush()
-        } catch {
-            Write-Log -Level Warning -Exception $_ -Message "Encountered exception while trying to flush telemetry events."
+        }
+        catch 
+        {
+            Write-Verbose -Message "Encountered exception while trying to flush telemetry events. Exception is: $_"
         }
     }
 }
