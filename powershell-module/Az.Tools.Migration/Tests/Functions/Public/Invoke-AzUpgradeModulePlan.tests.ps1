516c92a4bfcafecc3062533f5cf1ef71f66d1529
Keith Babinec <keithbabinec@users.noreply.github.com>
2020-09-15 23:31:48 -0700

    Add 'save to new files' feature support (#49)
    * Added save-to-new-files support
    
    * Implementation bug fix; updated docs
    
    * Updated quickstart to remove Find command and add notes around fileEditMode
    
    * More doc updates for editMode
    
    Co-authored-by: Dingmeng Xue <dixue@microsoft.com>

--- powershell-module/Az.Tools.Migration/Tests/Functions/Public/Invoke-AzUpgradeModulePlan.tests.ps1
+++ powershell-module/Az.Tools.Migration/Tests/Functions/Public/Invoke-AzUpgradeModulePlan.tests.ps1
@@ -2,7 +2,7 @@ Import-Module Az.Tools.Migration -Force
 
 InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
     Describe 'Invoke-AzUpgradeModulePlan tests' {
-        It 'Should be able to execute an update plan (single-file)' {
+        It 'Should be able to execute an update plan (single-file) to modify files in place' {
             # arrange
             $step = New-Object -TypeName UpgradePlan
             $step.FullPath = "C:\mock-file.ps1"
@@ -15,13 +15,49 @@ InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
 
             Mock -CommandName Invoke-ModuleUpgradeStep -ModuleName Az.Tools.Migration -MockWith { } -Verifiable
             Mock -CommandName Get-Content -MockWith { return "mock-file-contents" } -Verifiable
-            Mock -CommandName Set-Content -MockWith { } -Verifiable
+
+            # in 'ModifyExistingFiles' mode, we must specify a same path as the original file.
+            # mock verification below will validate that we indeed passed the correct path for this mode.
+            Mock -CommandName Set-Content -MockWith { } -ParameterFilter { $Path -eq "C:\mock-file.ps1" } -Verifiable
 
             # ensure we don't send telemetry during tests.
             Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
 
             # act
-            $result = Invoke-AzUpgradeModulePlan -Plan $step -Confirm:$false
+            $result = Invoke-AzUpgradeModulePlan -Plan $step -FileEditMode ModifyExistingFiles -Confirm:$false
+
+            # assert
+            $result | Should Not Be $null
+            $result.Count | Should Be 1
+            $result.GetType().FullName | Should Be 'UpgradeResult'
+            $result.UpgradeResult.ToString() | Should Be 'UpgradeCompleted'
+            $result.UpgradeSeverity.ToString() | Should Be 'Information'
+
+            Assert-VerifiableMock
+        }
+        It 'Should be able to execute an update plan (single-file) to save new files' {
+            # arrange
+            $step = New-Object -TypeName UpgradePlan
+            $step.FullPath = "C:\mock-file.ps1"
+            $step.UpgradeType = [UpgradeStepType]::Cmdlet
+            $step.PlanResult = [PlanResultReasonCode]::ReadyToUpgrade
+            $step.PlanSeverity = [DiagnosticSeverity]::Information
+            $step.Location = "mocked-file.ps1:10:5"
+            $step.Original = "Login-AzureRmAccount"
+            $step.Replacement = "Login-AzAccount"
+
+            Mock -CommandName Invoke-ModuleUpgradeStep -ModuleName Az.Tools.Migration -MockWith { } -Verifiable
+            Mock -CommandName Get-Content -MockWith { return "mock-file-contents" } -Verifiable
+
+            # in 'SaveChangesToNewFiles' mode, we must specify a new path (it doesn't match the original file)
+            # mock verification below will validate that we indeed passed the correct path for this mode.
+            Mock -CommandName Set-Content -MockWith { } -ParameterFilter { $Path -eq "C:\mock-file_az_upgraded.ps1" } -Verifiable
+
+            # ensure we don't send telemetry during tests.
+            Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
+
+            # act
+            $result = Invoke-AzUpgradeModulePlan -Plan $step -FileEditMode SaveChangesToNewFiles -Confirm:$false
 
             # assert
             $result | Should Not Be $null
@@ -74,13 +110,13 @@ InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
 
             Mock -CommandName Invoke-ModuleUpgradeStep -ModuleName Az.Tools.Migration -MockWith { } -Verifiable
             Mock -CommandName Get-Content -MockWith { return "mock-file-contents" } -Verifiable
-            Mock -CommandName Set-Content -MockWith { } -Verifiable
+            Mock -CommandName Set-Content -MockWith { }
 
             # ensure we don't send telemetry during tests.
             Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
 
             # act
-            $results = Invoke-AzUpgradeModulePlan -Plan $plan -Confirm:$false
+            $results = Invoke-AzUpgradeModulePlan -Plan $plan -FileEditMode ModifyExistingFiles -Confirm:$false
 
             # assert
             $results | Should Not Be $null
@@ -119,13 +155,13 @@ InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
 
             Mock -CommandName Invoke-ModuleUpgradeStep -ModuleName Az.Tools.Migration -MockWith { } -Verifiable
             Mock -CommandName Get-Content -MockWith { return "mock-file-contents" } -Verifiable
-            Mock -CommandName Set-Content -MockWith { } -Verifiable
+            Mock -CommandName Set-Content -MockWith { }
 
             # ensure we don't send telemetry during tests.
             Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
 
             # act
-            $results = Invoke-AzUpgradeModulePlan -Plan $plan -Confirm:$false
+            $results = Invoke-AzUpgradeModulePlan -Plan $plan -FileEditMode ModifyExistingFiles -Confirm:$false
 
             # assert
             $results | Should Not Be $null
@@ -167,13 +203,13 @@ InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
 
             Mock -CommandName Invoke-ModuleUpgradeStep -ModuleName Az.Tools.Migration -MockWith { } -Verifiable
             Mock -CommandName Get-Content -MockWith { return "mock-file-contents" } -Verifiable
-            Mock -CommandName Set-Content -MockWith { } -Verifiable
+            Mock -CommandName Set-Content -MockWith { }
 
             # ensure we don't send telemetry during tests.
             Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
 
             # act
-            $results = Invoke-AzUpgradeModulePlan -Plan $plan -Confirm:$false
+            $results = Invoke-AzUpgradeModulePlan -Plan $plan -FileEditMode ModifyExistingFiles -Confirm:$false
 
             # assert
             $results | Should Not Be $null
@@ -221,7 +257,7 @@ InModuleScope -ModuleName Az.Tools.Migration -ScriptBlock {
             Mock -CommandName Send-MetricsIfDataCollectionEnabled -ModuleName Az.Tools.Migration -MockWith { }
 
             # act
-            $results = Invoke-AzUpgradeModulePlan -Plan $plan -Confirm:$false
+            $results = Invoke-AzUpgradeModulePlan -Plan $plan -FileEditMode ModifyExistingFiles -Confirm:$false
 
             # assert
             $results | Should Not Be $null
